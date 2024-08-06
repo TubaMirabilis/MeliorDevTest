bac97e39e1286477d570d243432440a44ba2a21a
Benjamin Green <bkingsleygreen@gmail.com>
2024-08-06 09:56:53 +0100

    More elegant logic in AddMeliorServices method

--- Melior.InterviewQuestion/ServiceCollectionExtensions.cs
+++ Melior.InterviewQuestion/ServiceCollectionExtensions.cs
@@ -9,15 +9,13 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddMeliorServices(this IServiceCollection services)
     {
-        if (ConfigurationManager.AppSettings["DataStoreType"] == "Backup")
+        var sd = ConfigurationManager.AppSettings["DataStoreType"] switch
         {
-            services.AddScoped<IAccountDataStore, BackupAccountDataStore>();
-        }
-        else
-        {
-            services.AddScoped<IAccountDataStore, AccountDataStore>();
-        }
-        services.AddScoped<IPaymentService, PaymentService>();
+            "Backup" => new ServiceDescriptor(typeof(IAccountDataStore), typeof(BackupAccountDataStore), ServiceLifetime.Scoped),
+            _ => new ServiceDescriptor(typeof(IAccountDataStore), typeof(AccountDataStore), ServiceLifetime.Scoped)
+        };
+        services.Add(sd);
+        services.AddScoped<IPaymentService, PaymentService>();  
         return services;
     }
 }
